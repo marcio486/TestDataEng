cfb0165fe944fc862e2d0aa153c0983a7b469bd7
Marcio <marscz@alunos.utfpr.edu.br>
2019-11-12 14:34:33 -0300

    Unittests

--- DataFormat.py
+++ DataFormat.py
@@ -3,8 +3,9 @@ import numpy as np
 import requests
 
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="ProjetoTeste-b0a2e18f5677.json"
+#%%
 def loadDataDf():
-    
+    '''Carrega dados do CSV e cria/formata alguns campos '''
     res =  pd.read_csv('Pagamentos.csv',header =None,names = ['id','data','valorPago','tipoPlano'])
 
     res['vlrpago'] = res.valorPago.apply(lambda x: float(x[2:].replace(',','.'))) 
@@ -16,71 +17,83 @@ def loadDataDf():
     res['ano'] = res.data.apply(lambda x: x.year)
     return res
 
-def formatCl(keyC,ComprasCliente,cl):
-    for l in range(0,int(ComprasCliente.MesesPlano)):
-        x = (ComprasCliente.mes+l)%12
-        if x == 0 : x = 12
-        if ComprasCliente.mes+l >= 13:
-            try:
-                cl.at[keyC,'Mt_'+str(x)+'_'+str(ComprasCliente.ano+1)] = ComprasCliente.vlrpago/int(ComprasCliente.MesesPlano) + [cl.at[keyC,str(x)+'/'+str(ComprasCliente.ano+1)]][0]
-            except:
-                cl.at[keyC,'Mt_'+str(x)+'_'+str(ComprasCliente.ano+1)] = ComprasCliente.vlrpago/int(ComprasCliente.MesesPlano)
-            try:
-                cl.at[keyC,'Tipo_'+str(x)+'_'+str(ComprasCliente.ano+1)] = ComprasCliente.TipoPlano + cl.at[keyC,str(x)+'/'+str(ComprasCliente.ano+1)+' Tipo'][0]
-            except:
-                cl.at[keyC,'Tipo_'+str(x)+'_'+str(ComprasCliente.ano+1)] = ComprasCliente.TipoPlano
-        else:
-            try:
-                cl.at[keyC,'Mt_'+str(x)+'_'+str(ComprasCliente.ano)] = ComprasCliente.vlrpago/int(ComprasCliente.MesesPlano) + cl.at[keyC,str(x)+'/'+str(ComprasCliente.ano)][0]
-            except:
-                cl.at[keyC,'Mt_'+str(x)+'_'+str(ComprasCliente.ano)] = ComprasCliente.vlrpago/int(ComprasCliente.MesesPlano)
-            try:
-                cl.at[keyC,'Tipo_'+str(x)+'_'+str(ComprasCliente.ano)] = ComprasCliente.TipoPlano + cl.at[keyC,str(x)+'/'+str(ComprasCliente.ano)+' Tipo'] [0]
-            except:
-                cl.at[keyC,'Tipo_'+str(x)+'_'+str(ComprasCliente.ano)] = ComprasCliente.TipoPlano
-    return cl
-
+#%%
 def formatDataDf(startDf):
-    cl = pd.DataFrame()
-    cl['id'] = startDf.id.unique()
-    cl['Mt_8_2016'] = 0 # Para utilizar como mes 0
+    '''Com base nos nos dados do CSV gera DataFrame com os dados de compra do cliente '''
+    DadosClientes = pd.DataFrame()
+    DadosClientes['id'] = startDf.id.unique()
+    DadosClientes['Mt_8_2016'] = 0 # Para utilizar como mes 0
     
+    DadosClientes = createColumnsCl(startDf,DadosClientes)
+    DadosClientes = DadosClientes.set_index('id')
+     
+    gbc = startDf.groupby('id')#Agrupando por id's para auxiliar na sumarização por mês
+
+    for keyC in gbc.groups.keys():
+        grupoCliente = gbc.get_group(keyC)
+        DadosClientes = formatCl(keyC,grupoCliente,DadosClientes)#Popular colunas com valores de compras formatados
+            
+    DadosClientes = removeNaN(DadosClientes)
+    
+    DadosClientes = DadosClientes.reset_index()  
+    
+    return DadosClientes
+
+def createColumnsCl(startDf,cl):
+    '''Cria colunas dos meses para os clientes '''
     for i in sorted(startDf.ano.unique()):#Criando colunas necessárias
         for x in sorted(startDf[startDf.ano == i].mes.unique()):
             cl['Mt_'+str(x)+'_'+str(i)] = 0
             cl['Tipo_'+str(x)+'_'+str(i)] = ''
-            
-    cl = cl.set_index('id')
-     
-    gbc = startDf.groupby('id')#Agrupando por id's para auxiliar na sumarização por mês
-    # Valor i ex ->
-    #id                              0 i[1][0] 
-    #data          2018-12-05 00:00:00 i[1][1]
-    #valorPago               R$ 300,00 i[1][2] 
-    #tipoPlano                Bronze/3 i[1][3]
-    #vlrpago                       300 i[1][4]
-    #TipoPlano                  Bronze i[1][5]
-    #MesesPlano                      3 i[1][6]
-    #mes                            12 i[1][7] 
-    #dia                             5 i[1][8] 
-    #ano                          2018 i[1][9] 
-    for keyC in gbc.groups.keys():
-        grupoC = gbc.get_group(keyC)
-        for i in grupoC.iterrows():
-            ComprasCliente = i[1]
-            cl = formatCl(keyC,ComprasCliente,cl)#Popular colunas com valores de compras formatados
+    return cl   
+  
+def formatCl(keyC,grupoC,DadosClientes):
+    ''' Popula o dataframe que possui todas as informações de compra do cliente
+    verifica o e adiciona os valores a serem pagos e tipo do plano no mês
+    # Valor ComprasCliente ex ->
+    #id                              0 [0] 
+    #data          2018-12-05 00:00:00 [1]
+    #valorPago               R$ 300,00 [2] 
+    #tipoPlano                Bronze/3 [3]
+    #vlrpago                       300 [4]
+    #TipoPlano                  Bronze [5]
+    #MesesPlano                      3 [6]
+    #mes                            12 [7] 
+    #dia                             5 [8] 
+    #ano                          2018 [9]  '''
+    for i in grupoC.iterrows():
+            # i[1][x] = descrições da compra
+        ComprasCliente = i[1]
+        for l in range(0,int(ComprasCliente.MesesPlano)):
+            x = (ComprasCliente.mes+l)%12 #resto da divisão por 12
+            if x == 0 : x = 12            #caso seja divisivel por 12 é o mês 12
+            if ComprasCliente.mes+l >= 13:#Caso seja maior que 13 significa que são dados do ano seguinte 
+                      #ex:'Mt_'+str(1)+'_'+str(2018+1) = Mt_1_2019 -> dados de janeiro de 2019
+                DadosClientes.at[keyC,'Mt_'+str(x)+'_'+str(ComprasCliente.ano+1)] = ComprasCliente.vlrpago/int(ComprasCliente.MesesPlano)
+                
+                DadosClientes.at[keyC,'Tipo_'+str(x)+'_'+str(ComprasCliente.ano+1)] = ComprasCliente.TipoPlano
+            else:
+                                                                        #Ex: 300RS em um Plano de 3 meses = 100 reais por mês
+                DadosClientes.at[keyC,'Mt_'+str(x)+'_'+str(ComprasCliente.ano)] = ComprasCliente.vlrpago/int(ComprasCliente.MesesPlano)
+                
+                DadosClientes.at[keyC,'Tipo_'+str(x)+'_'+str(ComprasCliente.ano)] = ComprasCliente.TipoPlano
+    return DadosClientes
 
-    for col in cl.columns:
-        if 'Tipo' in col: cl[col]= cl[col].fillna('')
-        else: cl[col]= cl[col].fillna(0)
-
-    cl = cl.reset_index()   
-    return cl
+def removeNaN(DadosClientes):
+    '''Remove valores NaN '''
+    for col in DadosClientes.columns:
+        if 'Tipo' in col: DadosClientes[col]= DadosClientes[col].fillna('')
+        else: DadosClientes[col]= DadosClientes[col].fillna(0)  
+    return DadosClientes
+#%%
 
 def loadClientsInfo():
+    # pega informações extras do cliente
     r = requests.get(url = 'https://demo4417994.mockable.io/clientes/')
     return pd.DataFrame(r.json())
 
+#%%
+    
 def formatAndExportToBg(startDF,clientsInfo):
     aux = ''
     #df2 = pd.DataFrame()
@@ -140,7 +153,7 @@ def formatAndExportToBg(startDF,clientsInfo):
             #d = {'mesAno':' '.join(chosen_month.split('_')[1:3]),'MRR':df.MRR.sum(),'Expansion':df.Expansion.sum(),'Contraction':df.Contraction.sum(),'Canceled':df.Canceled.sum(),'TotalAnt':df.aux.sum(),'Ressurection':df.Ressurection.sum(),'Ativos':df.Ativos.sum()}
             #df2 = df2.append(pd.DataFrame(data =d,index = [colun]))
             aux = colun
-            
+#%%            
             
 startDf = loadDataDf()
 startDf = formatDataDf(startDf)
